1e27d31783fc95a65c4fd4cdcabade11c5fa19cd
jfzhou90 <burninghot4jesus2005@gmail.com>
2018-09-02 12:53:00 -0400

    bug fixes

--- client/src/components/DataTable/index.js
+++ client/src/components/DataTable/index.js
@@ -2,28 +2,5 @@
 // Components
 import DataTable from './dataTable';
 
-// Actions
-import { 
-  FETCH_PEOPLE, 
-  fetchPeople, 
-  ADD_PEOPLE, 
-  addPeople
-} from './actions';
-
-// Reducers
-import { 
-  PeopleReducer
-} from './reducers';
-
 //-------------- Exports ------------------//
 export {DataTable} ;
-
-export { 
-  FETCH_PEOPLE,
-  fetchPeople, 
-  ADD_PEOPLE, 
-  addPeople
-};
-export { 
-  PeopleReducer
-};
--- client/src/components/Reducers/index.js
+++ client/src/components/Reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import { PeopleReducer } from '../DataTable'
+import { PeopleReducer } from './reducers'
 
 export default combineReducers({
   people: PeopleReducer
